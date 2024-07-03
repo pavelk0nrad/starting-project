0d650abeba83b07eb35a3a290336ce31dc72872e
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-03 18:40:24 +0200

    add gitignore

---
+++ src/components/PostList.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Post from './Post';
+import classes from './PostList.module.css'
+import NewPost from './NewPost';
+
+function PostList({isPosting, onStopPosting}){
+    const [posts, setPosts] = useState([]);
+    
+    function addPostHandler(postData){
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+    return(
+    <>
+        {isPosting ? (
+        <Modal onClose={onStopPosting}>
+            <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal >
+    ) : null}
+        {posts.length > 0 && (
+        <ul className={classes.posts}>
+            {posts.map((post) => (
+                <Post key={post.body} author={post.author} body={post.body} />
+            ))}
+       
+        </ul>
+    )}
+    {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+            <h2>There are no posts yet.</h2>
+            <p>Start adding some! </p>
+        </div>
+    )}      
+</>
+
+    );
+}
+
+export default PostList;
